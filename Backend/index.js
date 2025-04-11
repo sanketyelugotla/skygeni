9f9d8751b43d8a57de2f2e9b363dcfe011d6ace9
sanket <sanketyelugotla123@gmail.com>
2025-04-11 21:25:30 +0530

    Mongo connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,31 @@
 const express = require("express");
+const connectDB = require("./config/db.js");
+const cors = require("cors"); // ✅ Make sure this line is present
+
+const app = express();
+
+const corsOptions = {
+    origin: ['https://digi-ballot.vercel.app', 'http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Middleware
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect DB
+connectDB();
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("Skygeni api running...");
+});
+
+
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
